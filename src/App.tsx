00db64856125c6482cb658dfe20eb6049ecb7bc9
kedeinroga <keybroga@gmail.com>
2021-02-25 15:51:59 -0500

    I can show target vaule with console

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from 'react';
 
-function App() {
+function App(): JSX.Element {
+  const [newTask, setNewTask] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <form>
+        <input type="text" onChange={e => console.log(e.target.value)} />
+      </form>
+    </Fragment>
   );
 }
 
