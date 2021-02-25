1e91b431df9d827ee1eb1353a34224cf3456eaa3
kedeinroga <keybroga@gmail.com>
2021-02-25 18:52:26 -0500

    giving html tags to the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,50 @@
-import { type } from 'os';
-import React, { Fragment, useState } from 'react';
+import React, { useState } from "react";
 
-type FromElement = React.FormEvent<HTMLFormElement>
+type FromElement = React.FormEvent<HTMLFormElement>;
 interface ITask {
   name: string;
   done: boolean;
 }
 
 function App(): JSX.Element {
-  const [newTask, setNewTask] = useState<string>('');
-  const [tasks, setTasks] = useState<ITask[]>([])
+  const [newTask, setNewTask] = useState<string>("");
+  const [tasks, setTasks] = useState<ITask[]>([]);
   const habdeleSubmit = (e: FromElement) => {
     e.preventDefault();
     addTask(newTask);
-    setNewTask('');
-  }
+    setNewTask("");
+  };
 
   const addTask = (name: string) => {
-    const newTasks: ITask[] = [...tasks, { name, done: false }]
-    setTasks(newTasks)
-  }
+    const newTasks: ITask[] = [...tasks, { name, done: false }];
+    setTasks(newTasks);
+  };
   return (
-    <Fragment>
-      <form onSubmit={habdeleSubmit} >
-        <input type="text" onChange={e => setNewTask(e.target.value)} value={newTask} />
-        <button>save</button>
-      </form>
-      {
-        tasks.map((t: ITask, i: number) => {
-          return <h1 key={i}>{t.name}</h1>
-        })
-      }
-    </Fragment>
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-body">
+              <form onSubmit={habdeleSubmit}>
+                <input
+                  className="form-control"
+                  type="text"
+                  onChange={(e) => setNewTask(e.target.value)}
+                  value={newTask}
+                  autoFocus
+                />
+                <button className="btn btn-success btn-block mt-2">save</button>
+              </form>
+            </div>
+          </div>
+          {tasks.map((t: ITask, i: number) => (
+            <div className="card card-body mt-2" key={i}>
+              <h2>{t.name}</h2>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
